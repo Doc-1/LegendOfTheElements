b80427dcecaa0618d9390152244b3a20c7eef3c7
_Doc <ldoc256@gmail.com>
2026-04-11 10:31:02 -0400

    Added a BlockEntityTickingSystem to handle ticking blocks a little easier.

---
+++ src/main/java/dev/docvin/legendofelements/blocks/entity/BlockEntityTickingSystem.java
@@ -0,0 +1,75 @@
+package dev.docvin.legendofelements.blocks.entity;
+
+import com.hypixel.hytale.component.*;
+import com.hypixel.hytale.component.query.Query;
+import com.hypixel.hytale.component.system.tick.EntityTickingSystem;
+import com.hypixel.hytale.math.util.ChunkUtil;
+import com.hypixel.hytale.math.vector.Vector3i;
+import com.hypixel.hytale.server.core.asset.type.blocktick.BlockTickStrategy;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.chunk.BlockComponentChunk;
+import com.hypixel.hytale.server.core.universe.world.chunk.WorldChunk;
+import com.hypixel.hytale.server.core.universe.world.chunk.section.BlockSection;
+import com.hypixel.hytale.server.core.universe.world.chunk.section.ChunkSection;
+import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
+
+import javax.annotation.Nonnull;
+
+public abstract class BlockEntityTickingSystem extends EntityTickingSystem<ChunkStore> {
+    private static final Query<ChunkStore> CHUNK_QUERY = Query.and(BlockSection.getComponentType(), ChunkSection.getComponentType());
+
+    @Override
+    public final void tick(float dt, int index, @Nonnull ArchetypeChunk<ChunkStore> archetypeChunk, @Nonnull Store<ChunkStore> store, @Nonnull CommandBuffer<ChunkStore> commandBufferChunk) {
+        BlockSection blocks = archetypeChunk.getComponent(index, BlockSection.getComponentType());
+
+        assert blocks != null;
+        if (blocks.getTickingBlocksCountCopy() != 0) {
+            ChunkSection section = archetypeChunk.getComponent(index, ChunkSection.getComponentType());
+            assert section != null;
+
+            WorldChunk worldChunk = commandBufferChunk.getComponent(section.getChunkColumnReference(), WorldChunk.getComponentType());
+            assert worldChunk != null;
+            assert worldChunk.getEntityChunk() != null;
+
+            World world = worldChunk.getWorld();
+
+            BlockComponentChunk blockComponentChunk = commandBufferChunk.getComponent(section.getChunkColumnReference(), BlockComponentChunk.getComponentType());
+            assert blockComponentChunk != null;
+
+            blocks.forEachTicking(blockComponentChunk, commandBufferChunk, section.getY(), (blockComponentChunk1, commandBuffer1, localX, localY, localZ, blockId) -> {
+                Ref<ChunkStore> blockRef = blockComponentChunk1.getEntityReference(ChunkUtil.indexBlockInColumn(localX, localY, localZ));
+                Archetype<ChunkStore> archetype;
+                if (blockRef == null)
+                    return BlockTickStrategy.IGNORED;
+                else
+                    archetype = commandBuffer1.getArchetype(blockRef);
+
+                if (this.getBlockQuery().test(archetype)) {
+                    this.blockTick(dt, blockId, archetype, blockRef, commandBuffer1, new Vector3i(localX, localY, localZ));
+                    return BlockTickStrategy.CONTINUE;
+                } else
+                    return BlockTickStrategy.IGNORED;
+            });
+        }
+    }
+
+    /**
+     * This method will run every tick for any block entity that matches the {@link BlockEntityTickingSystem#blockTick(float, int, Archetype, Ref, CommandBuffer, Vector3i)} provided
+     *
+     * @param dt            delta, the time delay between frames
+     * @param index         id of the block used to get the ref
+     * @param archetype     archetype of the block
+     * @param ref           reference of the block
+     * @param commandBuffer command buffer of the block
+     * @param targetBlock   local location of the block within the chunk
+     */
+    public abstract void blockTick(float dt, int index, Archetype<ChunkStore> archetype, @Nonnull Ref<ChunkStore> ref, @Nonnull CommandBuffer<ChunkStore> commandBuffer, @Nonnull Vector3i targetBlock);
+
+    public abstract Query<ChunkStore> getBlockQuery();
+
+    @Override
+    public final Query<ChunkStore> getQuery() {
+        return CHUNK_QUERY;
+    }
+}
+
